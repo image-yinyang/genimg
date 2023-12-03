2b6bf37ca3e92b7add8d29770f015c5aa6bd2788
Ed Fletcher <edfletcher@discordrc.com>
2023-12-02 19:17:12 -0800

    Reformat

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 import { Ai } from '@cloudflare/ai';
 
 async function _imageGen(requestId, env, ai, results, type) {
-	try {
-		const imageBytes = await ai.run('@cf/stabilityai/stable-diffusion-xl-base-1.0', { prompt: results[type].prompt });
-		const name = `${requestId}.${type}.png`;
-		const { etag, size } = await env.GEN_IMAGES_BUCKET.put(name, imageBytes);
-		console.log(`Object metadata for ${name}: ${JSON.stringify({ etag, size })}`);
-		return name;
-	} catch (e) {
-		console.log(`_imageGen failed for "${type}": ${e} [req: ${requestId}]`);
-		return null;
-	}
+    try {
+        const imageBytes = await ai.run('@cf/stabilityai/stable-diffusion-xl-base-1.0', { prompt: results[type].prompt });
+        const name = `${requestId}.${type}.png`;
+        const { etag, size } = await env.GEN_IMAGES_BUCKET.put(name, imageBytes);
+        console.log(`Object metadata for ${name}: ${JSON.stringify({ etag, size })}`);
+        return name;
+    } catch (e) {
+        console.log(`_imageGen failed for "${type}": ${e} [req: ${requestId}]`);
+        return null;
+    }
 }
 
 export default {
-	async queue(batch, env) {
-		const ai = new Ai(env.AI);
-		for (let message of batch.messages) {
-			const { requestId } = message.body;
-			console.log(`Processing request ${requestId}...`);
+    async queue(batch, env) {
+        const ai = new Ai(env.AI);
+        for (let message of batch.messages) {
+            const { requestId } = message.body;
+            console.log(`Processing request ${requestId}...`);
 
-			const reqObj = JSON.parse(await env.RequestsKVStore.get(requestId));
+            const reqObj = JSON.parse(await env.RequestsKVStore.get(requestId));
 
-			reqObj.results.good.imageBucketId = await _imageGen(requestId, env, ai, reqObj.results, 'good');
-			reqObj.results.bad.imageBucketId = await _imageGen(requestId, env, ai, reqObj.results, 'bad');
+            reqObj.results.good.imageBucketId = await _imageGen(requestId, env, ai, reqObj.results, 'good');
+            reqObj.results.bad.imageBucketId = await _imageGen(requestId, env, ai, reqObj.results, 'bad');
 
-			reqObj.status = 'processed';
-			await env.RequestsKVStore.put(requestId, JSON.stringify(reqObj));
-			const { success, meta } = await env.DB.prepare('insert into ByInputUrl values (?, ?);').bind(reqObj.input.url, requestId).run();
-			if (!success) {
-				console.error(`DB insert failure: ${JSON.stringify(meta)}`);
-			}
-			console.log(`Done with request ${requestId}`);
-		}
-	},
+            reqObj.status = 'processed';
+            await env.RequestsKVStore.put(requestId, JSON.stringify(reqObj));
+            const { success, meta } = await env.DB.prepare('insert into ByInputUrl values (?, ?);').bind(reqObj.input.url, requestId).run();
+            if (!success) {
+                console.error(`DB insert failure: ${JSON.stringify(meta)}`);
+            }
+            console.log(`Done with request ${requestId}`);
+        }
+    },
 };
